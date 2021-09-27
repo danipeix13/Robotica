cf32c3a2e67d61c4dcbdf017cb724b6f24410d0f
Dani Peix <dpeixdel@alumnos.unex.es>
2021-09-28 01:06:49 +0200

    straight forward

--- p2/src/specificworker.cpp
+++ p2/src/specificworker.cpp
@@ -1,5 +1,5 @@
 #include "specificworker.h"
-#include "cppitertools/ sliding_window.hpp"
+#include <chrono>
 
 /**
 * \brief Default constructor
@@ -30,59 +30,34 @@ void SpecificWorker::initialize(int period)
 		this->startup_check();
 	else
 		timer.start(Period);
+    std::srand(std::time(nullptr));
 }
 
 void SpecificWorker::compute()
 {
-    int stop_threshold = 650, slow_threshold = 1250, min_speed = 0, max_speed = 700, residue = 200, trim = 3; //COPPELIA VALUES
-//    float stop_threshold = 650.0, slow_threshold = 1250.0;
-//    float min_speed = 0.0, max_speed = 0.6;
-//    float residue = 0.1;
-//    float trim = 3.0;   // GIRAFF  VALUES
-    float adv = 0.8;
-    float rot = 0;
-    float m = (max_speed - min_speed) * 1. / (slow_threshold - stop_threshold);
-    float n = min_speed - m * stop_threshold + residue;
+    float stop_threshold = 650, residue = 200, trim = 3, adv = 1000, rot = 1.3;
 
-    if( auto ldata = laser_proxy->getLaserData(); !ldata.empty())
-    {
+    if( auto ldata = laser_proxy->getLaserData(); !ldata.empty()) {
         //Using only distance values
         std::vector<float> distances;
         for(auto &point : ldata)
             distances.push_back(point.dist);
-
-        //Filter, try to fix laser measure errors
-        if(distances[0] < 200)
-            distances[0] = 200;
-        for(auto &&window : iter::sliding_window(distances, 2))
-        {
-            if(window[1] < 200)
-                window[1] = window[0];
-        }
 
         //Sort and take the lower distance value
         int limit = distances.size()/trim;
         std::sort(distances.begin() + limit, distances.end() - limit, [](float a, float b){return a < b;});
         float minValue = distances[limit];
 
-        //Set the speeds depending on minValue, x1, x2, y1, y2
-        if(minValue < stop_threshold) {
-            rot = 0.8;
-            adv = min_speed;
-        }else if(minValue < slow_threshold)
-            adv = m * minValue + n;
-        else
-            adv = max_speed;
-
-        try
-        {
-            cout << "adv: " << adv << "     rot: " << rot << endl;
-            differentialrobot_proxy->setSpeedBase(adv, rot);
-        }
-        catch(const Ice::Exception &e)
-            { std::cout << e.what() << std::endl;}
-    }q
+        //Set the speeds depending on minValue SWITCH
+        if(minValue < stop_threshold) { //
+            differentialrobot_proxy->setSpeedBase(0, (std::rand()%2) ? rot : -rot);
+            std::this_thread::sleep_for(std::chrono::milliseconds(std::rand() % 2250));//random
+        } else
+            differentialrobot_proxy->setSpeedBase(adv, 0);
+    }
 }
+
+
 
 int SpecificWorker::startup_check()
 {
@@ -90,3 +65,46 @@ int SpecificWorker::startup_check()
 	QTimer::singleShot(200, qApp, SLOT(quit()));
 	return 0;
 }
+
+
+//int stop_threshold = 650, slow_threshold = 1250, min_speed = 50, max_speed = 700, residue = 200, trim = 3; //COPPELIA VALUES
+////    float stop_threshold = 650.0, slow_threshold = 1250.0;
+////    float min_speed = 0.0, max_speed = 0.6;
+////    float residue = 0.1;
+////    float trim = 3.0;   // GIRAFF  VALUES
+//float adv, rot = 0;
+//
+////We have to control the robot' speed depending on the distance for keeping its balance
+////Speed-Distance graphics
+//float m = (max_speed - min_speed) * 1. / (slow_threshold - stop_threshold);
+//float n = min_speed - m * stop_threshold + residue;
+//
+//if( auto ldata = laser_proxy->getLaserData(); !ldata.empty())
+//{
+////Using only distance values
+//std::vector<float> distances;
+//for(auto &point : ldata)
+//distances.push_back(point.dist);
+//
+////Sort and take the lower distance value
+//int limit = distances.size()/trim;
+//std::sort(distances.begin() + limit, distances.end() - limit, [](float a, float b){return a < b;});
+//float minValue = distances[limit];
+//
+////Set the speeds depending on minValue
+//if(minValue < stop_threshold) { //
+//rot = 0.8;
+//adv = min_speed;
+//}else if(minValue < slow_threshold)
+//adv = m * minValue + n;
+//else
+//adv = max_speed;
+//
+//try
+//{
+//cout << "adv: " << adv << "     rot: " << rot << endl;
+//differentialrobot_proxy->setSpeedBase(adv, rot);
+//}
+//catch(const Ice::Exception &e)
+//{ std::cout << e.what() << std::endl;}
+//}
