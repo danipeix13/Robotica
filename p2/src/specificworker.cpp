d8d002cbdd0527c604ce550859384eec83421099
Dani Peix <dpeixdel@alumnos.unex.es>
2021-09-27 23:46:42 +0200

    SW.cpp updated

--- p2/src/specificworker.cpp
+++ p2/src/specificworker.cpp
@@ -1,5 +1,5 @@
 #include "specificworker.h"
-#include "cppitertools/sliding_window.hpp"
+#include "cppitertools/ sliding_window.hpp"
 
 /**
 * \brief Default constructor
@@ -34,40 +34,54 @@ void SpecificWorker::initialize(int period)
 
 void SpecificWorker::compute()
 {
-    //dist < x1 --> dist = y1
-    //dist > x2 --> dist = y2
-    //else      --> dist = m * dist + n
-    //int x1 = 600, x2 = 1000, y1 = 0, y2 = 600, residue = 300, trim = 3; //COPPELIA VALUES
-    float x1 = 650.0, x2 = 1250.0, y1 = 0.0, y2 = 0.6, residue = 0.1, trim = 3.0;   //GIRAFF ROBO VALUES
-    float adv, rot = 0.8, m = (y2 - y1) * 1. / (x2 - x1) , n = y1 - m * x1 + residue;
+    int stop_threshold = 650, slow_threshold = 1250, min_speed = 0, max_speed = 700, residue = 200, trim = 3; //COPPELIA VALUES
+//    float stop_threshold = 650.0, slow_threshold = 1250.0;
+//    float min_speed = 0.0, max_speed = 0.6;
+//    float residue = 0.1;
+//    float trim = 3.0;   // GIRAFF  VALUES
+    float adv = 0.8;
+    float rot = 0;
+    float m = (max_speed - min_speed) * 1. / (slow_threshold - stop_threshold);
+    float n = min_speed - m * stop_threshold + residue;
 
-    if(auto ldata = laser_proxy->getLaserData(); !ldata.empty()) {
+    if( auto ldata = laser_proxy->getLaserData(); !ldata.empty())
+    {
         //Using only distance values
         std::vector<float> distances;
-        for(auto point : ldata)
+        for(auto &point : ldata)
             distances.push_back(point.dist);
 
         //Filter, try to fix laser measure errors
         if(distances[0] < 200)
             distances[0] = 200;
-        for(auto &&window : iter::sliding_window(distances, 2)){
+        for(auto &&window : iter::sliding_window(distances, 2))
+        {
             if(window[1] < 200)
                 window[1] = window[0];
         }
 
         //Sort and take the lower distance value
         int limit = distances.size()/trim;
-        std::sort(distances.begin() + limit, distances.end() - limit, [=](float a, float b){return a < b;});
+        std::sort(distances.begin() + limit, distances.end() - limit, [](float a, float b){return a < b;});
         float minValue = distances[limit];
 
         //Set the speeds depending on minValue, x1, x2, y1, y2
-        bool stop = minValue < x1, slow = minValue < x2;
-        adv = (stop) ? y1 : (slow) ? m * minValue + n : y2;
-        try{
-            std::cout << adv << std::endl;
-            differentialrobot_proxy->setSpeedBase(adv, (stop) ? rot : 0);
-        }catch(const Ice::Exception &e){ std::cout << e.what() << std::endl;}
-    }
+        if(minValue < stop_threshold) {
+            rot = 0.8;
+            adv = min_speed;
+        }else if(minValue < slow_threshold)
+            adv = m * minValue + n;
+        else
+            adv = max_speed;
+
+        try
+        {
+            cout << "adv: " << adv << "     rot: " << rot << endl;
+            differentialrobot_proxy->setSpeedBase(adv, rot);
+        }
+        catch(const Ice::Exception &e)
+            { std::cout << e.what() << std::endl;}
+    }q
 }
 
 int SpecificWorker::startup_check()
